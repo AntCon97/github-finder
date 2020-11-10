d3334518fd13c0367e678ee6ef394f7ac8cdc66a
Anthony <antcon1000@gmail.com>
2020-11-10 10:26:10 -0500

    added github api call, refactored some files

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class Navbar extends Component {
-  render() {
-    const { title, icon } = this.props;
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={icon} /> {title}
-        </h1>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ title, icon }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-gihub',
+};
 
 export default Navbar;
--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,40 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './userItem';
+import Spinner from './../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'kevwil',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/35?v=4',
-        html_url: 'https://github.com/kevwil',
-      },
-      {
-        id: '2',
-        login: 'atmos',
-        avatar_url: 'https://avatars3.githubusercontent.com/u/38?v=4',
-        html_url: 'https://github.com/atmos',
-      },
-      {
-        id: '3',
-        login: 'errfree',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/44?v=4',
-        url: 'https://api.github.com/users/errfree',
-      },
-    ],
-  };
-
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+
+Users.protoTyptes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
   display: 'grid',
